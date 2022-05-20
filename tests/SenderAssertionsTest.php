50bb666d94c1eca2c4affdfc6809c0c9d1b74bba
Brandon Ferens <brandon@kirschbaumdevelopment.com>
2022-05-20 10:52:28 -0700

    Sender assertions

--- tests/SenderAssertionsTest.php
+++ tests/SenderAssertionsTest.php
@@ -4,14 +4,32 @@ namespace Tests;
 
 use Symfony\Component\Mime\Email;
 use PHPUnit\Framework\ExpectationFailedException;
+use KirschbaumDevelopment\MailIntercept\AssertableMessage;
 
 class SenderAssertionsTest extends TestCase
 {
+    /*
+    |--------------------------------------------------------------------------
+    | assertMailSender
+    |--------------------------------------------------------------------------
+    */
+
     public function testMailSenderSingleEmail()
     {
         $email = $this->faker->email;
 
         $mail = (new Email())->sender($email);
+
+        $this->assertMailSender($email, $mail);
+    }
+
+    public function testMailSenderSingleEmailViaAssertableMessage()
+    {
+        $email = $this->faker->email;
+
+        $mail = new AssertableMessage(
+            (new Email())->sender($email)
+        );
 
         $this->assertMailSender($email, $mail);
     }
@@ -28,11 +46,28 @@ class SenderAssertionsTest extends TestCase
         $this->assertMailSender($email, $mail);
     }
 
+    /*
+    |--------------------------------------------------------------------------
+    | assertMailNotSender
+    |--------------------------------------------------------------------------
+    */
+
     public function testMailNotSenderSingleEmail()
     {
         $email = $this->faker->unique()->email;
 
         $mail = (new Email())->sender($this->faker->unique()->email);
+
+        $this->assertMailNotSender($email, $mail);
+    }
+
+    public function testMailNotSenderSingleEmailViaAssertableMessage()
+    {
+        $email = $this->faker->unique()->email;
+
+        $mail = new AssertableMessage(
+            (new Email())->sender($this->faker->unique()->email)
+        );
 
         $this->assertMailNotSender($email, $mail);
     }
